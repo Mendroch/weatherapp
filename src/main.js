013b0d5868297f50af8d66d2af62ee45e3a5e078
Krzysztof Mendroch <mendrochk@gmail.com>
2021-12-05 14:21:18 +0100

    Added request chain handling

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import {
+    getWeatherByCity,
+} from './apiService.js';
+
 const viewElems = {};
 
 const getDOMElem = (id) => {
@@ -33,7 +37,15 @@ const initializeApp = () => {
     setupListeners();
 }
 
-const onEnterSubmit = () => {}
+const onEnterSubmit = event => {
+    if(event.key === 'Enter') {
+        let query = viewElems.searchInput.value;
+        getWeatherByCity(query).then(data => {
+            console.log(data);
+        });
+    };
+}
+
 const onClickSubmit = () => {}
 
 document.addEventListener('DOMContentLoaded', initializeApp);
